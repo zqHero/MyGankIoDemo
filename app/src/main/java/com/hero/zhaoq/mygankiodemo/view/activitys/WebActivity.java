1951a42493d5ddc164457b13bafd403ee9d7cdfc
229457269@qq.com <zq229457269>
2017-04-11 15:16:30 +0800

    修改类名

--- app/src/main/java/com/hero/zhaoq/mygankiodemo/view/activitys/WebActivity.java
+++ app/src/main/java/com/hero/zhaoq/mygankiodemo/view/activitys/WebActivity.java
@@ -3,7 +3,6 @@ package com.hero.zhaoq.mygankiodemo.view.activitys;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
